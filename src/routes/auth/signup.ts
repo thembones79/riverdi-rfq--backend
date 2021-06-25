e8fb99a73ae97ab5a576dd67bc25fe2bdfbc3912
thembones79 <michal.szumnarski@gmail.com>
2021-06-25 15:09:43 +0200

    fix

--- src/routes/auth/signup.ts
+++ src/routes/auth/signup.ts
@@ -39,11 +39,13 @@ router.post(
       .withMessage("You must supply a RoleId"),
   ],
   validateRequest,
+
   async (req: Request, res: Response) => {
+    /*
     if (req.currentUser?.role_id !== 1) {
       throw new NotAuthorizedError();
     }
-
+*/
     const { email, password, username, shortname, role_id } = req.body;
     const existingUser = await UserRepo.findByEmail(email);
     if (existingUser) {
